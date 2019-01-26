f9b2abe15d0eed2207e2bd3cb7a00ff375e59c1f
jam1garner <jam1.mcleod@hotmail.com>
2019-01-26 13:07:36 -0500

    Add quality hardware mapping system

--- emu/emu.py
+++ emu/emu.py
@@ -9,36 +9,13 @@ def roundup(x):
     return int(math.ceil(x / 2048.0)) * 2048
 
 def hook_code(uc, address, size, user_data):
-    zr = uc.reg_read(UC_MIPS_REG_ZERO)
-    at = uc.reg_read(UC_MIPS_REG_AT)
-    v0 = uc.reg_read(UC_MIPS_REG_V0)
-    v1 = uc.reg_read(UC_MIPS_REG_V1)
-    a0 = uc.reg_read(UC_MIPS_REG_A0)
-    a1 = uc.reg_read(UC_MIPS_REG_A1)
-    a2 = uc.reg_read(UC_MIPS_REG_A2)
-    a3 = uc.reg_read(UC_MIPS_REG_A3)
-    t0 = uc.reg_read(UC_MIPS_REG_T0)
-    t1 = uc.reg_read(UC_MIPS_REG_T1)
-    t2 = uc.reg_read(UC_MIPS_REG_T2)
-    t3 = uc.reg_read(UC_MIPS_REG_T3)
-    t4 = uc.reg_read(UC_MIPS_REG_T4)
-    t5 = uc.reg_read(UC_MIPS_REG_T5)
-    t6 = uc.reg_read(UC_MIPS_REG_T6)
-    t7 = uc.reg_read(UC_MIPS_REG_T7)
-    s0 = uc.reg_read(UC_MIPS_REG_S0)
-    s1 = uc.reg_read(UC_MIPS_REG_S1)
-    s2 = uc.reg_read(UC_MIPS_REG_S2)
-    s3 = uc.reg_read(UC_MIPS_REG_S3)
-    s4 = uc.reg_read(UC_MIPS_REG_S4)
-    s5 = uc.reg_read(UC_MIPS_REG_S5)
-    s6 = uc.reg_read(UC_MIPS_REG_S6)
-    s7 = uc.reg_read(UC_MIPS_REG_S7)
-    gp = uc.reg_read(UC_MIPS_REG_GP)
-    sp = uc.reg_read(UC_MIPS_REG_SP)
     pc = uc.reg_read(UC_MIPS_REG_PC)
-    ra = uc.reg_read(UC_MIPS_REG_RA)
-    fp = uc.reg_read(UC_MIPS_REG_FP)
-    print(f"PC - {pc:08X}")
+    #print(f"PC - {pc:08X}")
+    if pc in range(0x8002D130, 0x8002D194):
+        print("---------------")
+        dump_regs(uc)
+    if pc == 0x8002D194:
+        quit()
     return True
 
 def dump_regs(uc):
@@ -71,14 +48,12 @@ def dump_regs(uc):
     pc = uc.reg_read(UC_MIPS_REG_PC)
     ra = uc.reg_read(UC_MIPS_REG_RA)
     fp = uc.reg_read(UC_MIPS_REG_FP)
-    print(f"v0 {v0:08X} v1 {v1:08X}")
     print(f"pc {pc:08X} sp {sp:08X}")
+    print(f"v0 {v0:08X} v1 {v1:08X}")
+    print(f"t4 {t4:08X} t3 {t3:08X}")
 
-def hook_mem_write(uc, access, address, size, value, user_data):
-    pc = uc.reg_read(UC_MIPS_REG_PC)
-    print(f"WRITE {value} at {hex(address)} ({size} bytes)  PC:{hex(pc)}")
-    return True
 
+print("FILELOAD firm_0_COACH_kern.elf")
 file_handle = open('./firm_0_COACH_kern.elf', 'rb')
 
 elf = ELFFile(file_handle)
@@ -88,15 +63,59 @@ entry = elf['e_entry']
 
 uc = Uc(UC_ARCH_MIPS, UC_MODE_32)
 
-# Needed mappings
+# Needed memory mappings
 # 80000180 - 8009FCB8 | r-x | .exception through .spc0.972___COUNTER__
 # 8009FCB8 - 800CA4C8 | rw- | .eh_frame through .bss
 # 90008000 - 90009C00 | -w- | .spd0 through .spd4
 # BFC08000 - BFC09C00 | rwx | .spc0 through .spc4
 
+# Needed hardware mappings
+# 0xb0400840 | some sort of LED?
+
 uc.mem_map(0x80000000, 0xD0000) # We have to map r-x and rw- together because unicorn :(
 uc.mem_map(0x90000000, 0xA000)
 uc.mem_map(0xBFC00000, 0xA000)
+
+# Map peripherials(?)
+PERIPHERIAL_LIST = [
+    ("unk_1", (0xB0400000, 0x1000)), #0xB0400840 is written to at the beginning
+    ("unk_2", (0xB0801000, 0x1000)), #0xB0801034 is written in CpuInit
+    ("unk_3", (0xB0802000, 0x1000))  # fake read from B0802040
+
+]
+
+
+def hook_mem_write(uc, access, address, size, value, user_data):
+    pc = uc.reg_read(UC_MIPS_REG_PC)
+    for p in PERIPHERIAL_LIST:
+        if address in range(p[1][0], p[1][0] + p[1][1]):
+            print(f"WRITE {value:08X} at {address:08X} ({size} bytes)  PC:{pc:08X} | PERIPHERIAL {p[0]}")
+            return True
+    print(f"WRITE {value:08X} at {address:08X} ({size} bytes)  PC:{pc:08X}")
+    return True
+
+def hook_mem_write_unmapped(uc, access, address, size, value, user_data):
+    pc = uc.reg_read(UC_MIPS_REG_PC)
+    print(f"WRITE UNMAPPED {value:08X} at {address:08X} ({size} bytes)  PC:{pc:08X}")
+    return True
+
+def hook_mem_read(uc, access, address, size, value, user_data):
+    pc = uc.reg_read(UC_MIPS_REG_PC)
+    for p in PERIPHERIAL_LIST:
+        if address in range(p[1][0], p[1][0] + p[1][1]):
+            print(f"READ {value:08X} at {address:08X} ({size} bytes)  PC:{pc:08X} | PERIPHERIAL {p[0]}")
+            return True
+    print(f"READ {value:08X} at {address:08X} ({size} bytes)  PC:{pc:08X}")
+    return True
+
+def hook_mem_read_unmapped(uc, access, address, size, value, user_data):
+    pc = uc.reg_read(UC_MIPS_REG_PC)
+    print(f"READ UNMAPPED {value:08X} at {address:08X} ({size} bytes)  PC:{pc:08X}")
+    return True
+
+for peripherial in PERIPHERIAL_LIST:
+    mapping = peripherial[1]
+    uc.mem_map(mapping[0], mapping[1])
 
 for i, section in enumerate(sections):
     if section["sh_addr"] == 0:
@@ -111,14 +130,21 @@ for i, section in enumerate(sections):
 
 def empty_func(a,b,c,d):
     pass
+# Map HOOK_CODE to an empty function, which fixes the fact unicorn's
+# reg read's PC value doesn't update after the first jal cause reasons???
+uc.hook_add(UC_HOOK_CODE, hook_code)
 
-uc.hook_add(UC_HOOK_CODE, empty_func)
-uc.hook_add(UC_HOOK_MEM_WRITE_UNMAPPED, hook_mem_write)
+uc.hook_add(UC_HOOK_MEM_WRITE, hook_mem_write)
+uc.hook_add(UC_HOOK_MEM_WRITE_UNMAPPED, hook_mem_write_unmapped)
+uc.hook_add(UC_HOOK_MEM_READ, hook_mem_read)
+uc.hook_add(UC_HOOK_MEM_READ_UNMAPPED, hook_mem_read_unmapped)
 
 print(f'JUMP Entrypoint: {entry:08X}')
 try:
     uc.emu_start(entry, 0x8009F6CC)
 except unicorn.UcError as e:
     print(f"ERROR: {e}")
+    dump_regs(uc)
+except:
     dump_regs(uc)
 file_handle.close()
